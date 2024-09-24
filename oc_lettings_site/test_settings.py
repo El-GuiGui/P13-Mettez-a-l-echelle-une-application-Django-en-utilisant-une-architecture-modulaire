66dd23adf8de7ee34d6fd73771a0dade7de4415b
Guillaume Lauroy <gui.lauroy@gmail.com>
2024-09-25 01:31:34 +0200

    add flake8 exclude

---
+++ oc_lettings_site/test_settings.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+# Settings pour les tests
+
+
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
